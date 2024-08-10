a06a6d7638cc365b20718afef12cc0d03df9996d
DeejLoaf <icecoldbrisktea@gmail.com>
2024-08-10 05:43:11 -0400

    Update LEADERBOARD.js

--- LEADERBOARD.js
+++ LEADERBOARD.js
@@ -1,3 +1,6 @@
+// Define the API URL
+const apiUrl = 'https://api.allorigins.win/get?url=https%3A//csgobig.com/api/partners/getRefDetails/Sav21faqfaslkhafsa%3Ffrom%3D1672534861000%26to%3D1702383132000';
+
 async function fetchAndDisplayLeaderboard() {
     try {
         // Fetch the data from the API using the proxy
@@ -72,3 +75,4 @@ async function fetchAndDisplayLeaderboard() {
 
 // Call the function to fetch and display leaderboard on page load
 fetchAndDisplayLeaderboard();
+
