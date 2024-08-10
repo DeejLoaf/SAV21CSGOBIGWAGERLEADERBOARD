db78a3d0805bbe2e0e93dd4608146909fed23363
DeejLoaf <icecoldbrisktea@gmail.com>
2024-08-10 05:47:28 -0400

    Update LEADERBOARD.js

--- LEADERBOARD.js
+++ LEADERBOARD.js
@@ -1,6 +1,5 @@
 // Define the API URL
 const apiUrl = 'https://api.allorigins.win/get?url=https%3A//csgobig.com/api/partners/getRefDetails/Sav21faqfaslkhafsa%3Ffrom%3D1672534861000%26to%3D1702383132000';
-
 async function fetchAndDisplayLeaderboard() {
     try {
         // Fetch the data from the API using the proxy
