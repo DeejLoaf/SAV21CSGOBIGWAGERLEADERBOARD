a185a829a3e91568311e7e454141f7b911d97003
DeejLoaf <icecoldbrisktea@gmail.com>
2024-11-03 13:25:25 -0500

    Update WAGERLEADERBOARD.js

--- WAGERLEADERBOARD.js
+++ WAGERLEADERBOARD.js
@@ -1,5 +1,5 @@
 // Define the API URL for your Google Sheets data 
-const apiUrl = 'https://script.googleusercontent.com/a/macros/bcgamerewards.com/echo?user_content_key=D8_mKHZE-l01bu78L7XnE8afE8gRcas48iEHbksrUKiEnS0lW7oVatKZlRs4UteL2CvrRUye-CDQvUpUcBeFdXt1AK7MlnLmOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMi80zadyHLKBprFhaOmYyEv5Fi9EJ8EgZjnFAkueCBXja49v4JU5efDDLvRdSRMyaKqEnxM9nnz5rz4mJxhr9_If2UyYxVLPRi9BTK8GOUXgPBZIkgXgsuxnYzAFYkbzXHuvZGyHs9Flcy6pr-Oj0FQ&lib=MrTXDmZhtezJ2YmKc4bRqSHPfFjUr1CYB'; // Replace with your web app URL
+const apiUrl = 'https://script.google.com/macros/s/AKfycbzy0cpKVVV56rufz9WG0MYkLpjuUH7Qbto-RMlQzf9PP529BpBGWIJbYTIEUHdKaaVi/exec'; // Replace with your web app URL
 
 async function fetchAndDisplayLeaderboard() {
     try {
